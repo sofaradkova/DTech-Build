1700dab949040444bb5eca6dade06be7c3021044
sofaradkova <123261570+sofaradkova@users.noreply.github.com>
2023-06-21 16:32:08 +0300

    Create boxes for features

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@
 import React from 'react'
 import './index.css'
 import Calendar from './components/Calendar'
+import SignIn from './components/SignIn'
+import EventCreate from './components/EventCreate'
 
 export default function App() {
   return (
-    <div>
-      <h1>Event Name</h1>
-      <h1>Gabys edit</h1>
-      <Calendar />
+    <div className='app-container'>
+      <div className='widgets-container'>
+        <SignIn />
+        <EventCreate />
+      </div>
+      <div>
+        <h1 className='calendar-name'>Event Name</h1>
+        <Calendar />
+      </div>
     </div>
   )
 }
